056d245d5860e3714b2fb56c19fef1af7c6f27d1
Sasha <roobel2005@gmail.com>
2021-10-07 23:06:46 +0300

    added method for getting user data

--- server/index.js
+++ server/index.js
@@ -1,14 +1,24 @@
 import express from "express";
+import { Linkedin } from "./services/linkedin";
+import { Fetch } from "./utils/fetch";
 
 const app = express();
 
-app.post("/linkedin/user/post", (req, res) => {
-  res.send("Hello World!");
+app.post("/linkedin/user/post", async (req, res) => {
+  const authorization = req.headers?.authorization;
+  const [, accessToken] = req.headers.authorization.split(" ");
+  if (!accessToken) {
+    return res.status(401).json();
+  }
+
+  const linkedin = new Linkedin(accessToken);
+  const user = await linkedin.getUser(Fetch);
+  res.send(user);
 });
 
 app.use((req, res, error, next) => {
   if (error) {
-    res.code(500).json({ error });
+    res.status(500).json({ error });
   }
 
   next();
--- server/services/linkedin.js
+++ server/services/linkedin.js
@@ -1,3 +1,17 @@
-class Linkedin {
+export class Linkedin {
+  constructor(token) {
+    this.token = token;
+  }
 
+  getUser(http) {
+    const url = "https://api.linkedin.com/v2/me";
+    const config = {
+      headers: {
+        "X-Restli-Protocol-Version": "2.0.0",
+        Authorization: `Bearer ${this.token}`,
+      },
+    };
+
+    return http(url, config);
+  }
 }
