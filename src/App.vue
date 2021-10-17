1626c7a50578d32ad122c09589892e90a799a732
Sasha <roobel2005@gmail.com>
2021-10-17 14:59:38 +0300

    Prepared template for work

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" style="width: 500px; height: 500px">
+    <linkedin/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import linkedin from "./components/linkedin";
+  export default {
+    components: {
+      linkedin: linkedin
+    }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/linkedin.vue
@@ -0,0 +1,16 @@
+<template>
+  <img :src="picture" alt="results" style="width: 100%"/>
+  <button>Send result</button>
+</template>
+
+<script>
+import Picture from "../assets/banana.png"
+export default {
+  data() {
+    return {
+      picture: Picture
+    }
+  },
+  name: 'App',
+}
+</script>
