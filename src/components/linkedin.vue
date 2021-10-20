a851a6b04f4844f586fa14479db8e09e044b0fb7
Sasha Karpovich <roobel2005@gmail.com>
2021-10-20 21:26:27 +0300

    added publishResults method template

--- src/components/linkedin.vue
+++ src/components/linkedin.vue
@@ -1,16 +1,21 @@
 <template>
-  <img :src="picture" alt="results" style="width: 100%"/>
-  <button>Send result</button>
+  <img :src="picture" alt="results" style="width: 100%" />
+  <button @click="publishResults">Publish results</button>
 </template>
 
 <script>
-import Picture from "../assets/banana.png"
+import Picture from "../assets/banana.png";
 export default {
   data() {
     return {
       picture: Picture
+    };
+  },
+  methods: {
+    publishResults() {
+      console.log("Publish results");
     }
   },
-  name: 'App',
-}
+  name: "Linkedin"
+};
 </script>
