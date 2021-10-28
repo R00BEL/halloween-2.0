2a3f59a94e80c7bdbae6b316f54846ff2c290dba
Sasha Karpovich <roobel2005@gmail.com>
2021-10-28 20:53:19 +0300

    added data retrieval from request

--- src/components/linkedin.vue
+++ src/components/linkedin.vue
@@ -1,5 +1,9 @@
 <template>
   <img :src="picture" alt="results" style="width: 100%" />
+  <p v-if="isError">
+    You refused to grant access rights. If you change your mind, reload the
+    page.
+  </p>
   <button @click="publishResults" :disabled="isButtonLocked">
     Publish results
   </button>
@@ -7,13 +11,14 @@
 
 <script>
 import Picture from "../assets/banana.png";
+import router from "../router";
 
 export default {
   data() {
     return {
       picture: Picture,
       isButtonLocked: true,
-      error: false
+      isError: false
     };
   },
   methods: {
@@ -22,7 +27,17 @@ export default {
     }
   },
   mounted() {
-    console.log("Start");
+    this.isError = !!this.$route.query.error;
+    if (this.isError) {
+      router.push("");
+      return;
+    }
+
+    const code = this.$route.query.code;
+    if (code) {
+      router.push("");
+      return;
+    }
   },
   name: "Linkedin"
 };
